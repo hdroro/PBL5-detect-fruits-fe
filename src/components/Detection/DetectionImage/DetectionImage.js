24e609b3a3962c689f05fa22fb44dedb1b2ca6f0
NguyenHa100903 <phuongha100903@gmail.com>
2024-04-17 14:48:05 +0700

    mHa chinh sua fe

--- src/components/Detection/DetectionImage/DetectionImage.js
+++ src/components/Detection/DetectionImage/DetectionImage.js
@@ -1,8 +1,9 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { ChevLeftIcon, ChevRightIcon } from "../../Icon/Icon";
 import "./DetectionImage.scss";
 
 function DetectionImage({ images }) {
+  const [currentIndex, setCurrentIndex] = useState(0);
   const imageRef = useRef();
   // const changeSrcImage1 = () => {
   //   console.log("imaeg1");
@@ -12,10 +13,25 @@ function DetectionImage({ images }) {
     console.log("image");
     imageRef.current.src = `data:image/png;base64,${image_path}`;
   };
+  const moveToNextImage = () => {
+    if (currentIndex < images["list_images"].length - 1) {
+      setCurrentIndex(currentIndex + 1);
+    }
+  };
+  const moveToPreviousImage = () => {
+    if (currentIndex > 0) {
+      setCurrentIndex(currentIndex - 1);
+    }
+  };
   return (
     <div className="detection-image-container">
       <div className="top-content d-flex justify-content-between align-items-center">
-        <div className="back-left">
+        <div
+          onClick={moveToPreviousImage}
+          className={
+            images && currentIndex > 0 ? "back-left" : "back-left disable"
+          }
+        >
           <ChevLeftIcon className="icon-action" />
         </div>
         <div className="image-detect-main">
@@ -23,12 +39,19 @@ function DetectionImage({ images }) {
             ref={imageRef}
             src={
               images &&
-              `data:image/png;base64,${images["list_images"][0]["image_path"]}`
+              `data:image/png;base64,${images["list_images"][currentIndex]["image_path"]}`
             }
             alt=""
           />
         </div>
-        <div className="back-right">
+        <div
+          className={
+            images && currentIndex < images["list_images"].length - 1
+              ? "back-right"
+              : "back-right disable"
+          }
+          onClick={moveToNextImage}
+        >
           <ChevRightIcon className="icon-action" />
         </div>
       </div>
